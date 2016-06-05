bc3f88002bb3e957e6e7fdb49145642ea1325ef2
YUSUFKILDAN <kildanyusuf@gmail.com>
2016-06-05 10:14:46 +0300

    Initial Commit

---
+++ FireSocial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FireSocial
+//
+//  Created by yusuf_kildan on 05/06/16.
+//  Copyright © 2016 yusuf_kildan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
